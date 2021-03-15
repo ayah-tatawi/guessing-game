94cff34530ad07d6f211bb06932cda926737ecb3
aya_tantawi <aya.mf5rah@gmail.com>
2021-03-15 23:27:03 +0200

    encomplet

---
+++ js/app.js
@@ -0,0 +1,57 @@
+'use strict';
+
+let yourName = prompt('enter yourname please');
+alert('welcome dear' +' ' + yourname);
+
+let mycolor = prompt('is my fav color is blue?');
+
+
+if ( mycolor === 'yes' )
+{
+    alert('wooow its true');
+
+}
+else 
+{
+    alert('nooo its not')
+ 
+}
+
+// if (mycolor === 'yes')
+// {
+ //   console.log(); ('you are correct')
+// }
+// else
+// {
+ //  console.log(); ('you are wrong')
+// }
+
+let myfood = prompt('is my fav food is mansaf?');
+if (myfood === 'yes')
+
+{
+    alert (yuuup its yummy!);
+}
+else
+{
+    alert(no thats wrong!);
+}
+// let myjob = prompt('is my job is an artist?);
+// if (myjob === 'yes')
+// {
+// console.log(); (thats wrong i cant draw);
+// }
+// else
+// {
+//  console.log();('thats right')
+// }
+
+let myphone = prompt('is my phone samsung or iphone?');
+if (myphone === 'samsung')
+{
+    alert ('yeees i love samsung!');
+}
+else 
+{
+    ('noo i only love samsung brand');
+}
